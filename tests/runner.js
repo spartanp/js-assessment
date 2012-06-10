281fe85f83ab0f5567ba1423bc2e448f8831acf6
Prathap Natarajan <pnatarajan@constantcontact.com>
2012-06-10 13:08:27 -0700

    more solutions

--- tests/runner.js
+++ tests/runner.js
@@ -2,10 +2,10 @@ var tests = [
   // link to test files here
 
   'tests/app/arrays',
-  'tests/app/objects',
+  'tests/app/objects',  
   'tests/app/functions',
-  'tests/app/async',
-  'tests/app/views'
+  'tests/app/async'
+  //'tests/app/views'
 ];
 
 require(tests, function() {
